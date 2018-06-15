50a09beab9954f81932cb6ac3531d7a7e22f2485
Johannes Christ <jc@jchri.st>
2018-06-15 16:20:11 +0200

    Add a handler for `GET`.

--- pysite/views/api/announcements.py
+++ pysite/views/api/announcements.py
@@ -1,15 +1,19 @@
-from flask import jsonify
-from schema import And, Schema, Optional
+from flask import abort, jsonify
+from schema import And, Optional, Schema
 
 from pysite.base_route import APIView
-from pysite.decorators import api_key, api_params, ValidationTypes
+from pysite.decorators import ValidationTypes, api_key, api_params
 from pysite.mixins import DBMixin, RMQMixin
 
 
-ANNOUNCEMENT_SCHEMA = Schema({
+GET_SCHEMA = Schema({
+    Optional('id'): str
+})
+
+POST_SCHEMA = Schema({
     'content': And(str, len, error="Announcement content must be a non-empty string"),
     'title': And(str, len, error="Announcement title must be a non-empty string"),
-    Optional('public', default=False): bool
+    Optional('public'): bool
 })
 
 
@@ -19,7 +23,49 @@ class AnnouncementManagementAPI(APIView, DBMixin, RMQMixin):
     table_name = "announcements"
 
     @api_key
-    @api_params(schema=ANNOUNCEMENT_SCHEMA, validation_type=ValidationTypes.json)
+    @api_params(schema=GET_SCHEMA, validation_type=ValidationTypes.params)
+    def get(self, params: dict):
+        """
+        Fetch all announcements from the database, returned as
+        a list of the announcements in the form:
+        [
+            {
+                'id': 'my-announcement-id',
+                'title': "Totally a serious announcement",
+                'content': "Example announcement content"
+            },
+            ...
+        ]
+        Keep in mind that if no announcements exists, this returns
+        an empty list.
+
+        If a parameter `id` is given, return the announcement
+        for the given ID in the following form:.
+        {
+            'id': 'totally-random-sequence-of-characters',
+            'title': "Definitely not an example announcement",
+            'content': "Absolutely meaningful and interesting content"
+        }
+        If no announcement with the given ID was found,
+        returns an empty dictionary:
+        {}
+
+        The ID must be provided as parameter, if exact lookup is desired.
+        API key must be provided as header.
+        """
+
+        if 'id' in params:
+            result = self.db.get(
+                self.table_name,
+                params['id']
+            ) or {}
+        else:
+            result = self.db.get_all(self.table_name)
+
+        return jsonify(result)
+
+    @api_key
+    @api_params(schema=POST_SCHEMA, validation_type=ValidationTypes.json)
     def post(self, announcement: dict):
         """
         Add a new announcement to the database.
@@ -35,6 +81,7 @@ class AnnouncementManagementAPI(APIView, DBMixin, RMQMixin):
         API key must be provided as header.
         """
 
+        announcement.setdefault('public', False)
         result = self.db.insert(
             self.table_name,
             announcement,
--- tests/test_api_announcements.py
+++ tests/test_api_announcements.py
@@ -3,12 +3,38 @@ import json
 from tests import SiteTest, app
 
 
-class AnnouncementsAPITests(SiteTest):
-    def test_can_create_normal_announcement(self):
-        post_data = json.dumps({
-            'title': "On the health effects of eating raw lemons",
-            'content': "For years, experts have wondered about effects of eating uncooked lemons."
-        })
+class EmptyDatabaseAnnouncementLookup(SiteTest):
+    def test_getting_all_announcements_returns_empty_list(self):
+        response = self.client.get(
+            '/announcements',
+            app.config['API_SUBDOMAIN'],
+            headers=app.config['TEST_HEADER']
+        )
+        self.assert200(response)
+        # FIXME: `SiteTest`s should probably be isolated from each other, but
+        #        they apparently are not. Once they are, check equality against
+        #        an empty list instead of checking for the class instance,
+        #        because currently, there might be other objects in the database.
+        self.assertIsInstance(response.json, list)
+
+    def test_exact_lookup_returns_empty_dict(self):
+        response = self.client.get(
+            '/announcements?id=whatever',
+            app.config['API_SUBDOMAIN'],
+            headers=app.config['TEST_HEADER'],
+        )
+        self.assert200(response)
+        self.assertEqual(response.json, {})
+
+
+class EmptyDatabaseAnnouncementCreation(SiteTest):
+    announcement_data = {
+        'title': "On the health effects of eating raw lemons",
+        'content': "For years, experts have wondered about effects of eating uncooked lemons."
+    }
+
+    def setUp(self):
+        post_data = json.dumps(self.announcement_data)
 
         response = self.client.post(
             '/announcements',
@@ -17,6 +43,20 @@ class AnnouncementsAPITests(SiteTest):
             headers=app.config['TEST_HEADER']
         )
 
-        self.assertEqual(response.status_code, 200)
+        self.assert200(response)
         self.assertEqual(response.json['status'], 'ok')
         self.assertIsInstance(response.json['created_id'], str)
+        self.created_id = response.json['created_id']
+
+    def test_can_lookup_announcement(self):
+        response = self.client.get(
+            f'/announcements?id={self.created_id}',
+            app.config['API_SUBDOMAIN'],
+            headers=app.config['TEST_HEADER']
+        )
+
+        self.assert200(response)
+        self.assertEqual(response.json['title'], self.announcement_data['title'])
+        self.assertEqual(response.json['content'], self.announcement_data['content'])
+        self.assertFalse(response.json['public'])
+        self.assertEqual(response.json['id'], self.created_id)
