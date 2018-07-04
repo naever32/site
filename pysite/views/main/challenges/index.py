87ea771f5ada61292b74dab795b459bf42a605ff
momothereal <momothereal.mc@gmail.com>
2018-07-04 15:51:06 -0400

    Actual filtering and pagination

--- pysite/views/main/challenges/index.py
+++ pysite/views/main/challenges/index.py
@@ -1,11 +1,16 @@
 import json
+import math
 from json import JSONDecodeError
 from typing import Dict
 
+import rethinkdb
 from flask import request, url_for
+from werkzeug.exceptions import NotFound
 
 from pysite.base_route import RouteView
 from pysite.mixins import DBMixin
+
+CHALLENGES_PER_PAGE = 4
 
 
 class ChallengesIndexView(RouteView, DBMixin):
@@ -15,16 +20,40 @@ class ChallengesIndexView(RouteView, DBMixin):
 
     def get(self):
         filter_raw = request.args.get("filter", default=str({}))
-        # todo: page_number = request.args.get("page", default=1, type=int)
+        page_number = request.args.get("page", default=1, type=int)
+
+        if page_number < 1:
+            raise NotFound()
+
+        page_slice = ((page_number - 1) * CHALLENGES_PER_PAGE, page_number * CHALLENGES_PER_PAGE)
 
         try:
             active_filter = json.loads(filter_raw)
         except JSONDecodeError:
             active_filter = {}
 
+        query_filter = {}
+
+        if "difficulty" in active_filter:
+            query_filter["difficulty"] = active_filter["difficulty"].lower()
+
+        challenge_filter_query = self.db.query(self.table_name).filter(
+            query_filter
+        )
+        challenge_filter_total_count_query = challenge_filter_query.count()
+        challenge_filter_sliced_query = challenge_filter_query.merge(
+            lambda challenge: {
+                "author": self.db.query("users").get(challenge["author_id"])
+            }
+        ).order_by(rethinkdb.desc("date")).slice(*page_slice).coerce_to("array")
+
+        total_page_count = math.ceil(self.db.run(challenge_filter_total_count_query) / CHALLENGES_PER_PAGE)
+        challenges = self.db.run(challenge_filter_sliced_query)
+
         return self.render(
             "main/challenges/index.html", filter=active_filter, filter_url=self.filter_url,
-            filter_match=self.filter_match
+            filter_match=self.filter_match, challenges=challenges, total_page_count=total_page_count,
+            pagination=self.pagination, page_number=page_number
         )
 
     def filter_url(self, current_filter: Dict, filter_changes: Dict):
@@ -47,3 +76,23 @@ class ChallengesIndexView(RouteView, DBMixin):
                 return False
 
         return True
+
+    def pagination(self, page, pages, active_filter):
+        result = [{"num": i,
+                   "url": url_for(self.name, page=i, filter=json.dumps(active_filter)),
+                   "active": page == i
+                   } for i in range(1, pages + 1)]
+        # add prev/next arrows
+        result.insert(0, {
+            "num": "previous",
+            "url": url_for(self.name, page=page - 1, filter=json.dumps(active_filter)) if page > 1 else "#",
+            "disabled": page == 1,
+            "active": False
+        })
+        result.append({
+            "num": "next",
+            "url": url_for(self.name, page=page + 1, filter=json.dumps(active_filter)) if page < pages else "#",
+            "disabled": page == pages,
+            "active": False
+        })
+        return result
