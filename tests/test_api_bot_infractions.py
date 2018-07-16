a219f6990a30a302c26200c15b9dd6fa7b426692
momothereal <momothereal.mc@gmail.com>
2018-07-16 17:08:48 -0400

    More work on the infractions API unit tests

--- tests/test_api_bot_infractions.py
+++ tests/test_api_bot_infractions.py
@@ -2,22 +2,24 @@ import json
 
 from tests import SiteTest, app
 
+TEST_USER_ID = "test"
+
 
 class ApiBotInfractionsEndpoint(SiteTest):
 
-    def test_infraction_create(self):
+    def test_infraction_create_invalid(self):
         # Invalid infraction type
         post_data_invalid_type = json.dumps(
-            {"type": "not_a_type", "reason": "test", "user_id": "abc", "actor_id": "abc"}
+            {"type": "not_a_type", "reason": "test", "user_id": TEST_USER_ID, "actor_id": TEST_USER_ID}
         )
         response = self.client.post("/bot/infractions", app.config["API_SUBDOMAIN"],
                                     headers=app.config["TEST_HEADER"],
                                     data=post_data_invalid_type)
         self.assert400(response)
 
-        # Kick infraction
+    def test_infraction_kick(self):
         post_data_valid = json.dumps(
-            {"type": "kick", "reason": "test", "user_id": "test", "actor_id": "test"}
+            {"type": "kick", "reason": "test", "user_id": TEST_USER_ID, "actor_id": TEST_USER_ID}
         )
         response = self.client.post("/bot/infractions", app.config["API_SUBDOMAIN"],
                                     headers=app.config["TEST_HEADER"],
@@ -25,12 +27,69 @@ class ApiBotInfractionsEndpoint(SiteTest):
         self.assert200(response)
         self.assertTrue("infraction" in response.json)
         self.assertTrue("id" in response.json["infraction"])
-        kick_infraction_id = response.json["infraction"]["id"]
-        response = self.client.get(f"/bot/infractions/id/{kick_infraction_id}", app.config["API_SUBDOMAIN"],
+        infraction_id = response.json["infraction"]["id"]
+        response = self.client.get(f"/bot/infractions/id/{infraction_id}", app.config["API_SUBDOMAIN"],
                                    headers=app.config["TEST_HEADER"])
         self.assert200(response)
         self.assertTrue("infraction" in response.json)
         self.assertTrue("id" in response.json["infraction"])
-        self.assertEqual(response.json["infraction"]["id"], kick_infraction_id)
+        self.assertEqual(response.json["infraction"]["id"], infraction_id)
         self.assertTrue("active" in response.json["infraction"])
         self.assertFalse(response.json["infraction"]["active"])
+
+    def test_infraction_ban(self):
+        post_data_valid = json.dumps(
+            {"type": "ban", "reason": "baddie", "user_id": TEST_USER_ID, "actor_id": TEST_USER_ID}
+        )
+        response = self.client.post("/bot/infractions", app.config["API_SUBDOMAIN"],
+                                    headers=app.config["TEST_HEADER"],
+                                    data=post_data_valid)
+        self.assert200(response)
+        self.assertTrue("infraction" in response.json)
+        self.assertTrue("id" in response.json["infraction"])
+        infraction_id = response.json["infraction"]["id"]
+
+        # Check if the ban is currently applied
+        response = self.client.get(f"/bot/infractions/user/{TEST_USER_ID}/ban/current", app.config["API_SUBDOMAIN"],
+                                   headers=app.config["TEST_HEADER"])
+        self.assert200(response)
+        self.assertTrue("infraction" in response.json)
+        self.assertIsNotNone(response.json["infraction"])
+        self.assertTrue("id" in response.json["infraction"])
+        self.assertEqual(response.json["infraction"]["id"], infraction_id)
+        self.assertIsNone(response.json["infraction"]["expires_at"])
+        self.assertTrue(response.json["infraction"]["active"])
+
+        # Update the expiration to 1d
+        patch_data_valid = json.dumps(
+            {"id": infraction_id, "duration": "1d"}
+        )
+        response = self.client.patch("/bot/infractions", app.config["API_SUBDOMAIN"],
+                                     headers=app.config["TEST_HEADER"],
+                                     data=patch_data_valid)
+        self.assert200(response)
+        self.assertTrue("success" in response.json)
+        self.assertTrue("infraction" in response.json)
+        self.assertTrue(response.json["success"])
+        self.assertIsNotNone(response.json["infraction"]["expires_at"])
+        self.assertTrue(response.json["infraction"]["active"])
+
+        # Disable the ban
+        patch_data_valid = json.dumps(
+            {"id": infraction_id, "active": False}
+        )
+        response = self.client.patch("/bot/infractions", app.config["API_SUBDOMAIN"],
+                                     headers=app.config["TEST_HEADER"],
+                                     data=patch_data_valid)
+        self.assert200(response)
+        self.assertTrue("success" in response.json)
+        self.assertTrue("infraction" in response.json)
+        self.assertTrue(response.json["success"])
+        self.assertFalse(response.json["infraction"]["active"])
+
+        # Check if there is no active ban anymore
+        response = self.client.get(f"/bot/infractions/user/{TEST_USER_ID}/ban/current", app.config["API_SUBDOMAIN"],
+                                   headers=app.config["TEST_HEADER"])
+        self.assert200(response)
+        self.assertTrue("infraction" in response.json)
+        self.assertIsNone(response.json["infraction"])
