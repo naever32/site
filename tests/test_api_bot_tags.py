573ef2ee05d0d8d4a376280b7f97e8950be9f44e
Tagptroll1 <thomas@galehuset.org>
2018-11-14 22:27:07 +0100

    Someones tests

--- tests/test_api_bot_tags.py
+++ tests/test_api_bot_tags.py
@@ -21,6 +21,11 @@ class ApiBotTagsEndpoint(SiteTest):
             'not_a_valid_key': 'gross_faceman'
         })
 
+        patch_data = json.dumps({
+            'tag_name': 'testing',
+            'tag_alias': 'test_alias'
+        })
+
         # POST method - no headers
         response = self.client.post('/bot/tags', app.config['API_SUBDOMAIN'])
         self.assertEqual(response.status_code, 401)
@@ -35,6 +40,25 @@ class ApiBotTagsEndpoint(SiteTest):
 
         # POST method - save tag
         response = self.client.post('/bot/tags', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER'], data=post_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {"success": True})
+
+        # PATCH method - no headers
+        response = self.client.patch('/bot/tags', app.config['API_SUBDOMAIN'])
+        self.assertEqual(response.status_code, 401)
+
+        # PATCH method - no data
+        response = self.client.patch('/bot/tags', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER'])
+        self.assertEqual(response.status_code, 400)
+
+        # PATCH method - bad data
+        response = self.client.patch('/bot/tags', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER'],
+                                     data=bad_data)
+        self.assertEqual(response.status_code, 400)
+
+        # PATCH method - add alias
+        response = self.client.patch('/bot/tags', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER'],
+                                     data=patch_data)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json, {"success": True})
 
@@ -56,6 +80,15 @@ class ApiBotTagsEndpoint(SiteTest):
         })
         self.assertEqual(response.status_code, 200)
 
+        # GET method - get from alias
+        response = self.client.get('/bot/tags?tag_alias=test_alias', app.config['API_SUBDOMAIN'],
+                                   headers=app.config['TEST_HEADER'])
+        self.assertEqual(response.json, {
+            "tag_alias": "test_alias",
+            "tag_name": "testing"
+        })
+        self.assertEqual(response.status_code, 200)
+
         # DELETE method - no headers
         response = self.client.delete('/bot/tags', app.config['API_SUBDOMAIN'])
         self.assertEqual(response.status_code, 401)
@@ -72,3 +105,10 @@ class ApiBotTagsEndpoint(SiteTest):
         response = self.client.delete('/bot/tags', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER'], data=get_data)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json, {"success": True})
+
+        # DELETE method - delete the testing alias
+        response = self.client.delete('/bot/tags', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER'],
+                                      data=get_data)
+        self.assertEqual(response.json, {"success": True})
+        self.assertEqual(response.status_code, 200)
+
